0321d96a19b5d9e83cc08d29b6ba09a908fc0b1d
markquiba <markjquiba@gmail.com>
2019-07-25 08:35:44 +0800

    Import and use ApolloProviders, Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import Dashboard from './pages/Dashboard';
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 function App() {
   return (
-    <div className="App">
-      Visitor's Logs
-    </div>
+    <ApolloProvider client={client}>
+      <ApolloHooksProvider client={client}>
+        <Router>
+          <div className="App">
+            <Link to="/">Visitor's Log</Link>
+            <Route exact path="/" component={Dashboard} />
+          </div>
+        </Router>
+      </ApolloHooksProvider>
+    </ApolloProvider>
   );
 }
 
